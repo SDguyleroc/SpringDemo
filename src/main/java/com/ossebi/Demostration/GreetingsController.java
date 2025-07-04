bd78807a038637a91949f0792b0eb8aaf481f105
Guy-leroc Ossebi <thonerguyleroc1@gmail.com>
2025-07-03 22:23:01 -0400

    GreetingsController controller added containing Get method returning "hello World"

--- src/main/java/com/ossebi/Demostration/GreetingsController.java
+++ src/main/java/com/ossebi/Demostration/GreetingsController.java
@@ -1,4 +1,19 @@
 package com.ossebi.Demostration;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GreetingsController {
+
+    
+    /**
+     * Handles GET requests to the /greet endpoint.
+     *
+     * @return A greeting message as a String
+     */
+    @GetMapping("/greet")
+    public String hello() {
+        return "Hello World";
+    }
 }
